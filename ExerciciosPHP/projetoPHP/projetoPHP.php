a1a22962a94268489729a7ececbbc7d9cf5f45b2
Eduardo Seguro <eduardojosesegurodeoliveira@gmail.com>
2024-09-03 11:47:33 -0300

    login completo, cadastro de usuário tá quase

--- ExerciciosPHP/projetoPHP/projetoPHP.php
+++ ExerciciosPHP/projetoPHP/projetoPHP.php
@@ -1,22 +1,41 @@
 <?php
-$users = [[
-    'user' => 'admin',
-    'password' => 'admin'
-]
-
+$users = [
+    [
+        'user' => 'admin',
+        'password' => 'admin'
+    ]
 ];
 
 $login = false;
 
 function registerUser(){
     global $users;
+    while(true){
+    $choice = readline("Deseja continuar? 1-Sim 2-Não\nDigite aqui: ");
     $user = readline ("Informe o usuário: ");
-    $password = readline ("Crie uma senha: ");
-    $passwordConfirm = readline ("Confirmar senha: ");
-    if($user == $users['user']){
+    $verifyUser = array_search($user, array_column($users, 'user'));
+    if($verifyUser != ''){
         echo "\nUsuário já existente, tente novamente!\n";
-        readline("Pressione ENTER para continuar . . .");
+        readline("Pressione ENTER para tentar novamente . . .");
     }
+    else{
+        $password = readline ("Crie uma senha: ");
+        $passwordConfirm = readline ("Confirmar senha: ");
+        if($passwordConfirm != $password){
+            readline("Senhas diferentes, pressione ENTER para tentar novamente . . .");
+        }
+        else{
+            $user = [
+                'user' => $user,
+                'password' => $password 
+            ];
+            $users[] = $user;
+            readline("Pressione ENTER para voltar ao menu. . .");
+            system('clear');
+            return menu();
+        }
+    }
+}
 }
 
 function menu(){
@@ -27,20 +46,20 @@ function menu(){
         $choice = readline("Digite a opção aqui: ");
         switch($choice){
             case 1:
-                
                 break;
             case 2:
-                
+                registerUser();
                 break;
             case 3:
                 
                 break;      
             case 4:
-                
+                return login();
                 break;   
             default:
                 echo "\nOpção inválida, tente novamente !\n";
                 readline("Pressione ENTER para continuar . . .");
+                system('clear');
                 break;       
         }
     }
@@ -49,16 +68,28 @@ function menu(){
 function login(){
     global $users;
     global $login;
+   
+    while(true){
     $user = readline("Digite o usuário: ");
-    $password = readline("Digite a senha: ");
-    if($user != $users['user'] or $password != $users['password']){
-        echo "\nUsuário ou senha incorretos!\nTente novamente . . .\n";
-        return login();
+    $confirmUser = array_search($user, array_column($users, 'user'));
+    if($confirmUser == ''){
+        readline("Usuário inexistente! Pressione ENTER para continuar . . .");
     }
     else{
-        $login = true;
-        menu();
+        $password = readline("Digite a senha: ");
+        if(in_array($password, $users[$confirmUser])){
+            $login = true;
+            system('clear');
+            return menu();
+        }   
+        else{
+            readline("Senha incorreta! Pressione ENTER para continuar . . .");
+        }     
     }
+    }
+    
+
+    
 } 
 
 login();
