74d6f50016edab934f54a20ace22e7df991b25d5
Eduardo Seguro <eduardojosesegurodeoliveira@gmail.com>
2024-09-09 10:50:57 -0300

    Falta só o sistema de troco

--- ExerciciosPHP/projetoPHP/projetoPHP.php
+++ ExerciciosPHP/projetoPHP/projetoPHP.php
@@ -1,23 +1,64 @@
 <?php
+date_default_timezone_set('America/Sao_Paulo');
+
 $users = [
     [
         'user' => 'admin',
         'password' => 'admin'
     ]
 ];
+$userLogin = '';
 
 $products = [];
 
+$logs = [];
+
+function timeDay(){
+    return date('d/m/Y H:i:s');
+}
 $login = false;
 $userLogin = '';
+$logsTXT = "Logs";
+
+
+function saveLogs($log)
+{
+    global $logs;
+    global $logsTXT;
+    $logsTXT = fopen("Logs.txt", "a");
+    $logs[] = $log;
+    fwrite($logsTXT, $log . PHP_EOL);
+    fclose($logsTXT);
+}
+
+function logView()
+{
+    global $logs;
+    while (true) {
+        system('clear');
+        echo "\nDeseja verificar o log? 1-Continuar 2-Voltar\n";
+        $choice = readline("Digite aqui: ");
+        if ($choice == 1) {
+            foreach ($logs as $log) {
+                echo $log . "\n";
+            }
+            readline("Precione ENTER para voltar . . .");
+        } else {
+            return menu();
+        }
+    }
+}
+
 
 function registerUser()
 {
     global $users;
     while (true) {
+        system('clear');
         echo "\nDeseja criar um usuário? 1-Continuar 2-Voltar\n";
         $choice = readline("Digite aqui: ");
         if ($choice == 1) {
+            system('clear');
             $user = readline("Informe o usuário: ");
             $verifyUser = array_search($user, array_column($users, 'user'));
             if ($verifyUser != '') {
@@ -34,6 +75,8 @@ function registerUser()
                         'password' => $password
                     ];
                     $users[] = $user;
+                    $log = "Cadastro realizado do usuário: ". $user['user'] . ", " . timeDay();
+                    saveLogs($log);
                     readline("Pressione ENTER para voltar ao menu. . .");
                     system('clear');
                     return menu();
@@ -49,9 +92,11 @@ function registerProduct()
 {
     global $products;
     while (true) {
+        system('clear');
         echo "\nDeseja cadastrar um produto? 1-Continuar 2-Voltar\n";
         $choice = readline("Digite aqui: ");
         if ($choice == 1) {
+            system('clear');
             $productName = readline("Informe o nome do produto: ");
             $verifyProduct = array_search($productName, array_column($products, 'name'));
             if ($verifyProduct != '') {
@@ -61,7 +106,7 @@ function registerProduct()
                 $productPrice = readline("Informe o preço: ");
                 $productQTD = readline("Informe a quantidade em estoque: ");
                 $newId = end($products);
-                $newId = $newId ? $newId[$newId]['id'] : 1;
+                $newId = $newId ? $newId['id'] + 1 : 1;
                 $product = [
                     'id' => $newId,
                     'name' => $productName,
@@ -69,6 +114,8 @@ function registerProduct()
                     'QTD' => $productQTD
                 ];
                 $products[] = $product;
+                $log = $productName . " adicionado ao estoque, " . timeDay();
+                saveLogs($log);
                 readline("Produto adicionado com sucesso, pressione ENTER para constinuar. . .");
                 system('clear');
             }
@@ -78,21 +125,110 @@ function registerProduct()
     }
 }
 
-function productDelete(){
+function productUpdate()
+{
+    global $products;
+    while (true) {
+        system('clear');
+        echo "\nDeseja editar um produto? 1-Continuar 2-Voltar\n";
+        $choice = readline("Digite aqui: ");
+        if ($choice == 1) {
+            foreach ($products as $product) {
+                echo "ID: " .  $product['id'] . " Nome: " . $product['name'] . " Preço: " . $product['price'] . " Estoque: " . $product['QTD'] . "\n";
+            }
+            $choiceProduct = readline("Digite o ID do produto que deseja editar: ");
+            $verifyProduct = array_search($choiceProduct, array_column($products, 'id'));
+            if ($verifyProduct == '') {
+                echo "\nProduto inexistente, tente novamente!\n";
+                readline("Pressione ENTER para tentar novamente . . .");
+            } else {
+                echo "\nO que deseja editar? 1-Nome 2-Preço 3-Quantidade\n";
+                $choiceEdit = readline("Digite aqui: ");
+                switch ($choiceEdit) {
+                    case 1:
+                        $newName = readline("Digite o novo nome: ");
+                        $products [$verifyProduct]['name'] = $newName;
+                        $log = "Nome do ID " . $verifyProduct + 1 . " modificado, " . timeDay();
+                        saveLogs($log);
+                        break;
+                    case 2:
+                        $newPrice = readline("Digite o novo preço: ");
+                        $products [$verifyProduct]['price'] = $newPrice;
+                        $log = "Preço do ID " . $verifyProduct + 1 . " modificado, " . timeDay();
+                        saveLogs($log);
+                        break;
+                    case 3:
+                        $newQTD = readline("Digite a quantidade: ");
+                        $products [$verifyProduct]['QTD'] = $newQTD;
+                        $log = "Quantidade do ID " . $verifyProduct + 1 . " modificada, " . timeDay();
+                        saveLogs($log);
+                        break;
+                    default:
+                        echo "\nOpção inválida, tente novamente !\n";
+                        readline("Pressione ENTER para continuar . . .");
+                        break;
+                }
+            }
+        } else {
+            return sellMenu();
+        }
+    }
+}
+
+function productDelete()
+{
     global $products;
     $productId = 0;
-    while(true){
+    while (true) {
+        system('clear');
         echo "\nDeseja remover um produto? 1-Continuar 2-Voltar\n";
         $choice = readline("Digite aqui: ");
         if ($choice == 1) {
-            $productId= readline("Informe o id do produto: ");
+            system('clear');
+            $productId = readline("Informe o id do produto: ");
             $verifyProduct = array_search($productId, array_column($products, 'id'));
             if ($verifyProduct == '') {
                 echo "\nID inválido, tente novamente!\n";
                 readline("Pressione ENTER para tentar novamente . . .");
             } else {
                 unset($products[$verifyProduct]);
+                $log = "Produto, ID " . $verifyProduct + 1 . " deletado, " . timeDay();
+                saveLogs($log);
                 readline("Produto removido com sucesso, pressione ENTER para constinuar. . .");
+                system('clear');
+            }
+        } else {
+            return sellMenu();
+        }
+    }
+}
+
+function sell(){
+    global $products;
+    $productId = 0;
+    while (true) {
+        system('clear');
+        echo "\nDeseja vender um produto? 1-Continuar 2-Voltar\n";
+        $choice = readline("Digite aqui: ");
+        if ($choice == 1) {
+            system('clear');
+            $productId = readline("Informe o id do produto: ");
+            $verifyProduct = array_search($productId, array_column($products, 'id'));
+            if ($verifyProduct == '') {
+                echo "\nID inválido, tente novamente!\n";
+                readline("Pressione ENTER para tentar novamente . . .");
+            } else {
+                $productQTD = readline("Informe quantos itens serão vendidos: ");
+                if($productQTD > $products[$verifyProduct]['QTD']){
+                    readline("Quantidade indisponível, pressione ENTER para constinuar. . .");
+                }
+                else{
+                    $products[$verifyProduct]['QTD'] -= $productQTD;
+                    $totalSell = $products[$verifyProduct]['price']*$productQTD;
+                    $log = "Venda: ID " . $verifyProduct + 1 . " QTD: $productQTD Valor da venda: $totalSell, " . timeDay();
+                    saveLogs($log);
+                    readline("Venda realizada com sucesso, pressione ENTER para constinuar. . .");
+                }
                 system('clear');
             }
         } else {
@@ -109,10 +245,11 @@ function sellMenu()
         foreach ($products as $product) {
             echo "ID: " .  $product['id'] . " Nome: " . $product['name'] . " Preço: " . $product['price'] . " Estoque: " . $product['QTD'] . "\n";
         }
-        echo "1-Vender 2-Admininstrar Produtos 3-Voltar\n";
+        echo "1-Vender 2-Gerenciar Produtos 3-Voltar\n";
         $choice = readline("Digite aqui: ");
         switch ($choice) {
             case 1:
+                return sell();
                 break;
             case 2:
                 $productMenus = true;
@@ -124,6 +261,7 @@ function sellMenu()
                             return registerProduct();
                             break;
                         case 2:
+                            return productUpdate();
                             break;
                         case 3:
                             return productDelete();
@@ -152,12 +290,12 @@ function sellMenu()
 
 function menu()
 {
-
+    global $userLogin;
     $choice = NULL;
     while (true) {
         system('clear');
         echo "=======CAIXA=======";
-        echo "\n1-Vender\n2-Cadastrar novo usuário\n3-Verificar Log\n4-Deslogar\n";
+        echo "\n1-Produtos\n2-Cadastrar novo usuário\n3-Verificar Log\n4-Deslogar\n";
         $choice = readline("Digite a opção aqui: ");
         switch ($choice) {
             case 1:
@@ -167,9 +305,11 @@ function menu()
                 return registerUser();
                 break;
             case 3:
-
+                return logView();
                 break;
             case 4:
+                $log = "Usuário $userLogin deslogou, " . timeDay();
+                saveLogs($log);
                 return login();
                 break;
             default:
@@ -183,10 +323,11 @@ function menu()
 
 function login()
 {
+    global  $userLogin;
     global $users;
     global $userLogin;
-
     while (true) {
+        system('clear');
         $userLogin = readline("Digite o usuário: ");
         $confirmUser = array_search($userLogin, array_column($users, 'user'));
         if ($confirmUser == '') {
@@ -194,6 +335,8 @@ function login()
         } else {
             $password = readline("Digite a senha: ");
             if (in_array($password, $users[$confirmUser])) {
+                $log = "Usuário $userLogin efetuou login, " . timeDay();
+                saveLogs($log);
                 system('clear');
                 return menu();
             } else {
@@ -202,5 +345,6 @@ function login()
         }
     }
 }
+
 
 login();
