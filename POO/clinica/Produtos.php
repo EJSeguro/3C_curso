3a8a2e64672e0f4fe564aacba9d533addfc3d254
EJSeguro <eduardojosesegurodeoliveira@gmail.com>
2024-09-13 11:11:45 -0300

    Produtos arrumado

--- POO/clinica/Produtos.php
+++ POO/clinica/Produtos.php
@@ -1,43 +1,48 @@
 <?php
 
-$listaProdutos=[];
 
-    class Produtos{
 
-        private $produto;
-        private $preco;
-        private $qtd;
-        public function __construct($produto, $preco, $qtd){
-            $produtos=[
-                $this->produto = $produto,
-                $this->preco = $preco,
-                $this->qtd = $qtd
-            ];
-            return $listaProdutos[]=$produtos;
-        }
+class Produtos
+{
 
-        public function getProduto(){
-            return $this->produto;
-        }
+    private $produto;
+    private $preco;
+    private $qtd;
+    public function __construct($produto, $preco, $qtd)
+    {
 
-        public function getPreco(){
-            return $this->preco;
-        }
-
-        public function getQTD(){
-            return $this->qtd;
-        }
-
-        public function setQTD($valor){
-            $this->qtd = $valor;
-        }
-
-        public function setPreco($valor){
-            $this->preco = $valor;
-        }
-
-        public function setProduto($valor){
-            $this->produto = $valor;
-        }
-
+        $this->produto = $produto;
+        $this->preco = $preco;
+        $this->qtd = $qtd;
     }
+
+    public function getProduto()
+    {
+        return $this->produto;
+    }
+
+    public function getPreco()
+    {
+        return $this->preco;
+    }
+
+    public function getQTD()
+    {
+        return $this->qtd;
+    }
+
+    public function setQTD($valor)
+    {
+        $this->qtd = $valor;
+    }
+
+    public function setPreco($valor)
+    {
+        $this->preco = $valor;
+    }
+
+    public function setProduto($valor)
+    {
+        $this->produto = $valor;
+    }
+}
--- POO/clinica/index.php
+++ POO/clinica/index.php
@@ -7,14 +7,13 @@
     $produtos = [];
     $vendas = [];
     while(true){
-        
+
     }
     $humano = new Humano($nome, $idade, $endereco, $contato);
     $funcionario = new Funcionario($humano, $cargo, $salario);
     $animal = new Animal($nome, $raca, $qtdPatas, $cor, $peso, $tamanho, $humano);
 
 
-    
 
 
 
