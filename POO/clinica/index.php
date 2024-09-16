ceac5a9b5ddcf82f6e8d6efffc6944b4625b32fe
Eduardo <eduardojosesegurodeoliveira@gmail.com>
2024-09-16 01:36:58 -0300

    Corpo menu

--- POO/clinica/index.php
+++ POO/clinica/index.php
@@ -1,20 +1,44 @@
 <?php
-    require_once "Animal.php";
-    require_once "Humano.php";
-    require_once "Funcionario.php";
-    
 
-    $produtos = [];
-    $vendas = [];
-    while(true){
+require_once "Animal.php";
+require_once "Humano.php";
+require_once "Funcionario.php";
 
+$produtos = [];
+$vendas = [];
+function voltandoMenu()
+{
+    echo "Voltando ao menu!\n";
+    readline("Pressione ENTER para continaur ...");
+}
+
+function opcaoInvalida()
+{
+    echo "Opção inválida, voltando ao menu!\n";
+    readline("Pressione ENTER para continaur ...");
+}
+
+
+
+while (true) {
+    echo "\n========Clínica Veterinária========\n1-Clientes\n2-Animais\n2-Funcionários\n3-Produtos\n";
+    $escolha = readline("Digite aqui: ");
+    switch ($escolha) {
+        case 1:
+            
+            break;
+        case 2:
+            
+            break;
+        case 3:
+            break;
+        case 4:
+            break;
+        default:
+            echo "Opção inválida!\n";
+            readline("Pressione ENTER para tentar novamente ...");
+            break;
     }
-    $humano = new Humano($nome, $idade, $endereco, $contato);
-    $funcionario = new Funcionario($humano, $cargo, $salario);
-    $animal = new Animal($nome, $raca, $qtdPatas, $cor, $peso, $tamanho, $humano);
+}
 
 
-
-
-
-    
