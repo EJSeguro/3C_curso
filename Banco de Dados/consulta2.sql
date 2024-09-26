e5d7b159d394695c8e5039715bd7503b63a48108
EJSeguro <eduardojosesegurodeoliveira@gmail.com>
2024-09-26 09:01:54 -0300

    lista de consulta 2 terminada

--- Banco de Dados/consulta2.sql
+++ Banco de Dados/consulta2.sql
@@ -9,13 +9,18 @@ INNER JOIN vendedor
 ON  venda.codVendedor = vendedor.codVendedor
 WHERE status = 'Despachada';
 
-SELECT * FROM clienteFisico; -----SEI LÁ----- 
+SELECT clienteFisico.nome FROM clienteFisico JOIN cliente ON clienteFisico.codCliente = cliente.codCliente WHERE endereco = 'Rua Marechal Floriano, 56';
 
 SELECT * FROM clienteJuridico;
 
-5) SEI LÁ
+SELECT clienteJuridico.nomeFantasia, cliente.endereco, cliente.telefone, cidade.nome, cidade.siglaEStado FROM clienteJuridico
+INNER JOIN cliente
+ON clienteJuridico.codCliente = cliente.codCliente
+INNER JOIN cidade
+ON  cliente.codCidade = cidade.codCidade
+WHERE cliente.dataCadastro BETWEEN 01/01/1999 AND 30/06/2003;  
 
-6)  SEI LÁ
+SELECT vendedor.nome FROM vendedor JOIN venda ON venda.codVendedor = vendedor.codVendedor JOIN clienteJuridico ON venda.codCliente = clienteJuridico.codCliente WHERE clienteJuridico.nomeFantasia = 'Gelinski';
 
 SELECT produtoLote.codProduto, numeroLote, produto.descricao FROM produtoLote
 INNER JOIN produto
